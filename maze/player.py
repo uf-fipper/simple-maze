63a93a4ae6c52ad4c7ee9f7cdbd0ae2e2815e7c9
fish huaji <m139687499@126.com>
2022-07-19 23:21:41 +0800

    完成player模块

---
+++ maze/player.py
@@ -0,0 +1,16 @@
+from .point import Point
+
+
+class Player:
+    def __init__(self, pos: Point, name: str = ''):
+        """
+        初始化一个玩家
+        :param pos: 初始位置
+        :param name: 名字（其实没用）
+        """
+        self.pos = pos
+        self.name = name
+        self.step = 0
+        """移动步数"""
+        self.move_times = 0
+        """移动次数"""
