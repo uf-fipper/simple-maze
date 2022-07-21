a819fbe7a314fd06ea73de0dc337199ff8ebc407
fish huaji <m139687499@126.com>
2022-07-21 13:41:11 +0800

    增加类型提示

--- maze/point.py
+++ maze/point.py
@@ -1,7 +1,13 @@
 from collections import namedtuple
+from typing import Generic, TypeVar
+
+_T_p = TypeVar('_T_p', bound=int)
 
 
-class Point(namedtuple('Point', 'x y')):
+class Point(namedtuple('Point', 'x y'), Generic[_T_p]):
+    def __new__(cls, x: _T_p, y: _T_p):
+        return super().__new__(cls, x, y)
+
     def get_range(self):
         return [
             Point(self[0] - 1, self[1]),
@@ -9,3 +15,8 @@ class Point(namedtuple('Point', 'x y')):
             Point(self[0], self[1] - 1),
             Point(self[0], self[1] + 1),
         ]
+
+    def __add__(self, other) -> "Point":
+        if isinstance(other, (Point, tuple)):
+            return Point(self[0] + other[0], self[1] + other[1])
+        return NotImplemented
