668a633be50552736421ead13901d6d6b0fcd11e
fish huaji <m139687499@126.com>
2022-07-22 21:54:30 +0800

    增加move_player方法，并增加一些异常

--- maze/exceptions.py
+++ maze/exceptions.py
@@ -2,7 +2,23 @@ class MazeException(Exception):
     pass
 
 
-class MapInitException(MazeException):
+class MapException(MazeException):
+    pass
+
+
+class MapInitException(MapException):
+    pass
+
+
+class MapIndexError(MapException, IndexError):
+    pass
+
+
+class GameException(MazeException):
+    pass
+
+
+class GameMoveException(GameException):
     pass
 
 
