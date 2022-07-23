baf7ae79883cb746a2ec7ba17cedbe192be6b99c
fish huaji <m139687499@126.com>
2022-07-24 00:46:19 +0800

    优化OnShowContainer: 如果找不到实例的方法，就去找类的

--- maze/onshowcontainer.py
+++ maze/onshowcontainer.py
@@ -1,6 +1,6 @@
-from typing import Callable, TypeVar, Optional, Generic, Hashable
+from typing import Callable, TypeVar, Optional, Generic, Hashable, Union, Type
 
-_T = TypeVar('_T', bound=Hashable)
+_T = Hashable
 _T_result = TypeVar('_T_result')
 _T_func = Callable[[_T], _T_result]
 
@@ -13,12 +13,14 @@ class OnShowContainer(Generic[_T_result]):
         def add(func: _T_func):
             self.obj_funcs[source_obj] = func
 
-        return add
+        return add   
 
     def __getitem__(self, source_obj: _T) -> _T_result:
         return self.get_object(source_obj)
 
     def get_object(self, source_obj: _T) -> _T_result:
-        if source_obj not in self.obj_funcs:
-            raise TypeError('please set a function first')
-        return self.obj_funcs[source_obj](source_obj)
+        if source_obj in self.obj_funcs:
+            return self.obj_funcs[source_obj](source_obj)
+        if type(source_obj) in self.obj_funcs:
+            return self.obj_funcs[type(source_obj)](source_obj)
+        raise TypeError('please set a function first')
